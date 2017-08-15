7779c1a4980d1570f8aa723eca27417b6db7cd77
Andrew Keesler <andrew.keesler@silabs.com>
2017-08-14 20:18:52 -0400

    Add boilerplate for sensor/sink apps.
    I am too lazy to write the actual apps right now...

---
+++ cpp/app/sensor_sink/sensor_app.cpp
@@ -0,0 +1,10 @@
+//
+// sensor_app.cpp
+//
+// Sink application
+//
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
---
+++ cpp/app/sensor_sink/sink_app.cpp
@@ -0,0 +1,10 @@
+//
+// sink_app.cpp
+//
+// Sink application
+//
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
